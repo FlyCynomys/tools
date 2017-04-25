11b85fa274beccc14cfc859409b7e3221cb7054a
grapestree <435594427@qq.com>
2017-04-25 10:58:16 +0800

    time format function

--- format/format.go
+++ format/format.go
@@ -2,7 +2,10 @@ package format
 
 import (
 	"strconv"
+	"time"
 )
+
+const timeFormat = "2006-01-02 15:04:05"
 
 func ToInt(v interface{}) int {
 	if v == nil {
@@ -24,4 +27,27 @@ func ToInt64(v interface{}) int64 {
 		return 0
 	}
 	return num
+}
+
+func FormatTimeFromTi(template string, timepoint time.Time) string {
+	if template == "" {
+		return time.Now().Format(timeFormat)
+	}
+	return time.Now().Format(template)
+}
+
+func FormatTimeFromUnix(template string, timepoint int64) string {
+	ti := time.Unix(timepoint, 0)
+	if template == "" {
+		return ti.Format(timeFormat)
+	}
+	return ti.Format(template)
+}
+
+func FormatTimeToUnix(timepoint string) int64 {
+	ti, err := time.Parse(timeFormat, timepoint)
+	if err != nil {
+		return -1
+	}
+	return ti.Unix()
 }
--- format/format_test.go
+++ format/format_test.go
@@ -1,6 +1,9 @@
 package format
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestToInt(t *testing.T) {
 	type args struct {
@@ -37,6 +40,68 @@ func TestToInt64(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := ToInt64(tt.args.v); got != tt.want {
 				t.Errorf("ToInt64() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFormatTimeFromTi(t *testing.T) {
+	type args struct {
+		template  string
+		timepoint time.Time
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := FormatTimeFromTi(tt.args.template, tt.args.timepoint); got != tt.want {
+				t.Errorf("FormatTimeFromTi() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFormatTimeFromUnix(t *testing.T) {
+	type args struct {
+		template  string
+		timepoint int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := FormatTimeFromUnix(tt.args.template, tt.args.timepoint); got != tt.want {
+				t.Errorf("FormatTimeFromUnix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFormatTimeToUnix(t *testing.T) {
+	type args struct {
+		timepoint string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := FormatTimeToUnix(tt.args.timepoint); got != tt.want {
+				t.Errorf("FormatTimeToUnix() = %v, want %v", got, tt.want)
 			}
 		})
 	}
